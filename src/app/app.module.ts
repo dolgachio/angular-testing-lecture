db06c2a529e8a0c9ac4691d0b9c238ab96b99996
Stanislav Dolgachov <stanislav_dolgachov@epam.com>
2018-06-25 22:31:47 +0300

    Remove redundant component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { LoginWidgetComponent } from './login-widget/login-widget.component';
 import { LoginWidgetWithServiceComponent } from './login-widget-with-service/login-widget-with-service.component';
 import { LoginWidgetWithTestbedComponent } from './login-widget-with-testbed/login-widget-with-testbed.component';
 import { LoginWidgetWithTestbedAndDependencyComponent } from './login-widget-with-testbed-and-dependency/login-widget-with-testbed-and-dependency.component';
-import { LoginWidgetTestCasesComponent } from './login-widget-test-cases/login-widget-test-cases.component';
 import { PublicationCardComponent } from './publication-card/publication-card.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserPictureComponent } from './user-profile/user-picture/user-picture.component';
@@ -21,7 +20,6 @@ import { PublicationLogService } from './publication-card/publication-log.servic
     LoginWidgetWithServiceComponent,
     LoginWidgetWithTestbedComponent,
     LoginWidgetWithTestbedAndDependencyComponent,
-    LoginWidgetTestCasesComponent,
     PublicationCardComponent,
 
     UserProfileComponent,
