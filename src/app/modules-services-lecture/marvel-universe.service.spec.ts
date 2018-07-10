3acb501736a54583d44c70109c23f52fde081b47
Stanislav Dolgachov <s.dolgachov@gmail.com>
2018-07-10 09:50:00 +0300

    Add tests for hero marvel service

--- src/app/modules-services-lecture/marvel-universe.service.spec.ts
+++ src/app/modules-services-lecture/marvel-universe.service.spec.ts
@@ -1,15 +1,33 @@
-import { TestBed, inject } from '@angular/core/testing';
-
 import { MarvelUniverseService } from './marvel-universe.service';
 
 describe('MarvelUniverseService', () => {
+  let sut: MarvelUniverseService;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MarvelUniverseService]
-    });
+    sut = new MarvelUniverseService();
   });
 
-  it('should be created', inject([MarvelUniverseService], (service: MarvelUniverseService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should return the most powerful hero', () => {
+    const mockHeroes = [
+        {
+          name: 'Ant-man',
+          power: 90,
+        },
+        {
+          name: 'Dr. Banner',
+          power: 10,
+        },
+        {
+          name: 'Spider-man',
+          power: 150,
+        }
+      ];
+    const expectedResult = {
+      name: 'Spider-man',
+      power: 150,
+    };
+    sut.heroes = mockHeroes;
+
+    expect(sut.getTheStrongestHero()).toEqual(expectedResult);
+  });
 });
